f26b1c1765ec6be9858db820dd5a0f3b1e1febf1
Spronk <evgexascherbakov@mail.ru>
2024-10-16 13:20:43 +0300

    Semi-final of Lab 2 -> task 1,working filtration for autist user, need a little fix, but IT`S WORKING!!!

--- Lab #2 Local/1 задание.cpp
+++ Lab #2 Local/1 задание.cpp
@@ -7,80 +7,93 @@
 int task_1()
 {
     int matrixsize, line, column, determ, sumgtr, sumvtr;
+    int b = 1;
     int sumgl = 0;
     int sumvt = 0;
     int matrix[4][4];
-   
-        printf("Input size of matrix: ");
+   while(b)
+   {
+       printf("\nInput size of matrix: ");
 
-        if (scanf_s("%d", &matrixsize) != 1) {rewind(stdin); printf("Wrong type of data!");}
-        else {printf("\nInput matrix size of %d:\n", matrixsize);}
-        if (matrixsize == 0) { printf("\nNo such matrix exists"); return 0; }
-        if (matrixsize == 1) { printf("The determinant of this matrix is ​​equal to the number itself"); return 0; }
-        if (matrixsize > 5) { printf("This programm can't calculate this"); return 0; }
-        
+       if (scanf_s("%d", &matrixsize) != 1) { rewind(stdin); printf("Wrong type of data!"); }
+       else if (matrixsize <= 0) { rewind(stdin); printf("\nNo such matrix exists");  }
+       else if (matrixsize == 1) { rewind(stdin); printf("The determinant of this matrix is ​​equal to the number itself");  }
+       else if (matrixsize > 4) { rewind(stdin); printf("This programm can't calculate this");  } // fix!
+       else { printf("\nInput matrix with size of %d:\n", matrixsize); }
 
-        for (line = 0; line < matrixsize; line++)
-        {
-            for (column = 0; column < matrixsize; column++)
-            {
-                int n = scanf_s("%d", &matrix[line][column]);
-                if (n!=1)
-                {
-                    printf("\nWrong type of data!");
-                    exit(1);
-                }
-            }
-        }
-       
+       for (line = 0; line < matrixsize; line++)
+       {
+           for (column = 0; column < matrixsize; column++)
+           {
+               int n = scanf_s("%d", &matrix[line][column]);
+               if (n != 1)
+               {
+                   b = 0;
+               }
+           }
+       }
+       if (b == 0)
+       {
+           printf("\nWrong type of data!");
+       }
 
 
-    if (matrixsize == 2)
-    {
-      
-       
-        for (line = 0; line < matrixsize; line++)
-            for (column = 0; column < matrixsize; column++)
-            {
-                if (line == column)
-                {
-                    sumgl += matrix[line][column];
-                }
 
-                if (line + column == matrixsize - 1)
-                {
-                    sumvt += matrix[line][column];
-                }
-            }
-        determ = sumgl - sumvt;
-        printf("\nMatrix determinant size of %d = %d", matrixsize, determ);
-    }
- 
-    
+       if ((matrixsize == 2) && (b != 0))
+       {
 
-    if (matrixsize == 3 && scanf_s("%d", &matrix[line][column]) == 1)
-    {
-        for (line = 0; line < matrixsize; line++)
-            for (column = 0; column < matrixsize; column++)
-            {
-                if (line == column)
-                {
-                    sumgl += matrix[line][column];
-                }
 
-                if (line + column == matrixsize - 1)
-                {
-                    sumvt += matrix[line][column];
-                }
+           for (line = 0; line < matrixsize; line++)
+               for (column = 0; column < matrixsize; column++)
+               {
+                   if (line == column)
+                   {
+                       sumgl += matrix[line][column];
+                   }
 
-                sumgtr = matrix[0][1] + matrix[1][2] + matrix[2][0] + matrix[1][0] + matrix[2][1] + matrix[0][2];
-                sumvtr = matrix[1][2] + matrix[2][1] + matrix[0][0] + matrix[1][0] + matrix[0][1] + matrix[2][2];
-                determ = sumgl + sumgtr - sumvt + sumvtr;
-            }
+                   if (line + column == matrixsize - 1)
+                   {
+                       sumvt += matrix[line][column];
+                   }
+               }
+           determ = sumgl - sumvt;
+           printf("\nMatrix determinant size of %d = %d", matrixsize, determ);
 
-        printf("Matrix determinant size of %d = %d", matrixsize, determ);
-    }
-    
+           printf("\nFor continue press 1, for exit 0 - ");
+           scanf_s("%d", &b);
+       }
+
+
+
+       if ((matrixsize == 3) && (b != 0))
+       {
+           for (line = 0; line < matrixsize; line++)
+               for (column = 0; column < matrixsize; column++)
+               {
+                   if (line == column)
+                   {
+                       sumgl += matrix[line][column];
+                   }
+
+                   if (line + column == matrixsize - 1)
+                   {
+                       sumvt += matrix[line][column];
+                   }
+
+                   sumgtr = matrix[0][1] + matrix[1][2] + matrix[2][0] + matrix[1][0] + matrix[2][1] + matrix[0][2];
+                   sumvtr = matrix[1][2] + matrix[2][1] + matrix[0][0] + matrix[1][0] + matrix[0][1] + matrix[2][2];
+                   determ = sumgl + sumgtr - sumvt + sumvtr;
+               }
+
+           printf("Matrix determinant size of %d = %d", matrixsize, determ);
+
+           printf("\nFor continue press 1, for exit 0 - ");
+           scanf_s("%d", &b);
+       }
+
+   }
+   return 0;
+
 }
 
 int main()
